f41493b3a31727573c836f1b949c603103aee0aa
joriesieck <jsieck@globalcognition.org>
2020-01-06 15:21:07 -0500

    hooked in scss stuff

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-import './styles/styles.css';
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />,document.getElementById('app'));
